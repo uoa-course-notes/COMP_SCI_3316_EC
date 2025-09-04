6027dd85863f367c2bb182de02fcdd8567fa689e
Tang Piseth <tangpiseth43@gmail.com>
2025-09-04 18:28:34 +0930

    A: Still not familiarized

--- ioh_experiment/main.py
+++ ioh_experiment/main.py
@@ -3,14 +3,15 @@ from ioh import get_problem, ProblemClass, logger
 import numpy as np 
 
 
-def random_search(func, budget=10):
+np.random.seed(42)
+def random_search(func, budget = 10):
     for i in range(budget):
         x = np.random.uniform(func.bounds.lb, func.bounds.ub)
         func(x)
 
 # help(problem)
 
-np.random.seed(42)
+
 
 # create a default logger which writes to folder 'temp'
 folder_name = 'temp'
@@ -23,17 +24,17 @@ print(f"Problem type = {f}")
 random_search(f)
 
 # this can then be attached to the problem
-f.attach_logger(l)
+# f.attach_logger(l)
 
 
 
 
 # f = get_problem(1, 1, 5, ProblemClass.PBO)
-print(f"meta_data = {f.meta_data}")
-print(f"state = {f.state}")
-print(f"Evaluated = {f([0,0,0,0,0])}")
+# print(f"meta_data = {f.meta_data}")
+# print(f"state = {f.state}")
+# print(f"Evaluated = {f([0,0,0,0,0])}")
 
-f.reset()
+# f.reset()
 
 
 
