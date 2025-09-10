72a6a0855507e53e075ab0cfb78e4a7f1ceee177
Tang Piseth <tangpiseth43@gmail.com>
2025-09-10 18:41:33 +0930

    A: Fixed venv dependencies issue

--- ioh_experiment/main.py
+++ ioh_experiment/main.py
@@ -3,25 +3,25 @@ from ioh import get_problem, ProblemClass, logger
 import numpy as np 
 
 
-np.random.seed(42)
-def random_search(func, budget = 10):
-    for i in range(budget):
-        x = np.random.uniform(func.bounds.lb, func.bounds.ub)
-        func(x)
+# np.random.seed(42)
+# def random_search(func, budget = 10):
+#     for i in range(budget):
+#         x = np.random.uniform(func.bounds.lb, func.bounds.ub)
+#         func(x)
 
 # help(problem)
 
 
 
 # create a default logger which writes to folder 'temp'
-folder_name = 'temp'
-l = logger.Analyzer(folder_name=folder_name)
+# folder_name = 'temp'
+# l = logger.Analyzer(folder_name=folder_name)
 
-f = get_problem(1, 1, 5, ProblemClass.PBO)
-print(f"Problem type = {f}")
+# f = get_problem(1, 1, 5, ProblemClass.PBO)
+# print(f"Problem type = {f}")
 
-# run the algorithm and store data
-random_search(f)
+# # run the algorithm and store data
+# random_search(f)
 
 # this can then be attached to the problem
 # f.attach_logger(l)
